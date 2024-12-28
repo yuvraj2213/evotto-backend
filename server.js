11925c298a6a27654e07a0c89498e831370b3785
Yuvraj Preet <yuvraj13preet@gmail.com>
2024-12-28 22:47:16 +0530

    cors link changed

--- server.js
+++ server.js
@@ -1,52 +1,51 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors=require("cors")
+const cors = require("cors");
 
 const connectDB = require("./config/db");
 const authRoute = require("./routes/authRoutes");
 const adminRoute = require("./routes/adminRoutes");
 const feedbackRoute = require("./routes/feedbackRoutes");
-const rentalVehicleRoute=require("./routes/rentalVehicleRoutes")
-const secondHandCarRoute=require("./routes/secondHandCarRoutes")
-const slideshowRoute=require("./routes/slideshowRoutes")
+const rentalVehicleRoute = require("./routes/rentalVehicleRoutes");
+const secondHandCarRoute = require("./routes/secondHandCarRoutes");
+const slideshowRoute = require("./routes/slideshowRoutes");
 const errorMiddleware = require("./middlewares/error-mw");
 const vehicleRoute = require("./routes/vehicleRoutes");
 
 dotenv.config();
 const app = express();
 
+// Updated CORS options
 const corsOptions = {
   origin: [
-    'https://evotto.in'
-    // 'https://evotto-nfh4.onrender.com',
+    'https://www.evotto.in'  // Corrected CORS origin
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Allow cookies if needed
 };
 
-app.use(cors(corsOptions))
-
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
-
+// API Routes
 app.use("/api/auth", authRoute);
 app.use("/api/form/", feedbackRoute);
-app.use("/api/data/",rentalVehicleRoute)
-app.use("/api/data/",secondHandCarRoute)
-app.use("/api/data/",slideshowRoute)
-app.use("/api/data/",vehicleRoute)
+app.use("/api/data/", rentalVehicleRoute);
+app.use("/api/data/", secondHandCarRoute);
+app.use("/api/data/", slideshowRoute);
+app.use("/api/data/", vehicleRoute);
 
 // Admin Routes
-app.use("/api/admin",adminRoute);
-
+app.use("/api/admin", adminRoute);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Evotto Backend");
 });
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
+// Connect to database and start server
 connectDB().then(() => {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
