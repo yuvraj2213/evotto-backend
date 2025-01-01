74dd4532cda0400edc3e3c2a1772d056a12151b5
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-01-01 16:07:01 +0530

    cors link changed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 const corsOptions = {
   origin: [
     // 'http://localhost:5173',
-    'https://www.evotto.in'
+    'https://www.evotto.in',
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Allow cookies if needed
