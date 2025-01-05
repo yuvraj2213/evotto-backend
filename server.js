80a6ca759a64aa91d3243450e76f1f0f3398cd18
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-01-06 00:51:03 +0530

    Style changes

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use('/images', express.static(path.join(__dirname, '/tmp')));
+app.use('/images', express.static(path.join(__dirname, '../uploads')));
+
 
 
 // API Routes
