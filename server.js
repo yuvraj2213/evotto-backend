8ec2de463e763f13e92999214416ca33d15b16d0
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-01-29 21:28:51 +0530

    cors link change

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ const upload = multer({ storage });
 
 // CORS configuration
 const corsOptions = {
-  // origin: "https://www.evotto.in",
-  origin: "http://localhost:5173",
+  origin: "https://www.evotto.in",
+  // origin: "http://localhost:5173",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
